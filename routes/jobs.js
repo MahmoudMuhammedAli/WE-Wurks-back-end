a779b5e260bc04557527d76737bb5985c26f3d5e
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-05-27 15:41:28 +0200

    delete and get jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,16 +1,38 @@
 const express = require("jobs");
 const router = express.Router();
+import Job from "../models/jobs"
 
 router.post("/", (req, res) => {
   // add job code goes here
 });
 
 router.get("/:id", (req, res) => {
-  // get job code with a specific id goes here
+  console.log('getting all jobs');
+  Book.find({})
+    .exec(function(err, jobs) {
+      if(err) {
+        res.send('cant get the jobs ')
+      } else {
+        console.log(jobs);
+        res.json(jobs);
+      }
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update jobs code goes here
 });
 
+router.delete("/:id", (req, res) => {
+  Job.findOneAndRemove({
+    _id: req.params.id
+  }, function(err, job) {
+    if(err) {
+      res.send('unable to remove the job')
+    } else {
+      console.log(job);
+      res.status(204);
+    }
+  });
+})
 module.exports = router;
