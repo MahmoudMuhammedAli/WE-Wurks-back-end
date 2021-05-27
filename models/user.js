f360b01ef168cd2b010fff26ffcb07de5b148f2a
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-05-27 15:33:59 +0200

    add user-manger

--- models/user.js
+++ models/user.js
@@ -5,8 +5,10 @@ const UserSchema = new Schema({
   name: String,
   email: { type: String,required: true ,unique: true },
   password: String,
+  isManager:boolean,
 });
+this.email==="mahmoud@gmail.com"?  this.isManager = true:this.isManager=false
+
 
 const User = mongoose.model("User", UserSchema);
-
 module.exports = User;
