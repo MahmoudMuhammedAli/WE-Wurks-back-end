1b437ad00a87ffad0260944c392d9fda7a5343a8
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-05-27 15:25:32 +0200

    email model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: String,
-  email: { type: String, unique: true },
+  email: { type: String,required: true ,unique: true },
   password: String,
 });
 
